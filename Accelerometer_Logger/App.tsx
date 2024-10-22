079f249132a8ee239f83fac3895ae6e1d01c28f1
Caleb Kornegay <caleb.kornegay@gmail.com>
2024-10-22 17:51:39 -0400

    Adding sensor logging and timestamping the difference in updates

--- Accelerometer_Logger/App.tsx
+++ Accelerometer_Logger/App.tsx
@@ -1,20 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Accelerometer, AccelerometerMeasurement } from 'expo-sensors';
+import { useState, useEffect } from 'react';
+
+let start = Date.now()
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [{ x, y, z, timestamp }, setData] = useState<AccelerometerMeasurement>({
+        x: 0,
+        y: 0,
+        z: 0,
+        timestamp: 0
+    });
+    const [timediff, setTimediff] = useState<number>(0.0);
+
+    const updateinterval = 1;
+
+    Accelerometer.setUpdateInterval(updateinterval);
+    Accelerometer.addListener(setData);
+
+    useEffect(() => {
+        const diff = (Date.now() - start) / 1000;
+        start = Date.now();
+        setTimediff(diff);
+        // console.log(diff, timestamp, start);
+    }, [x, y, z, setData])
+
+    return (
+        <View style={styles.container}>
+            <Text>Timediff: {timediff}s</Text>
+            <Text>x: {x}</Text>
+            <Text>y: {y}</Text>
+            <Text>z: {z}</Text>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
