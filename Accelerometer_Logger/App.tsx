888fb18ffa6746900d0bf1cf37132ad6783be45f
Connor Gannaway <charlesconnorgannaway@gmail.com>
2024-10-23 13:21:29 -0400

    update

--- Accelerometer_Logger/App.tsx
+++ Accelerometer_Logger/App.tsx
@@ -1,9 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 import { Accelerometer, AccelerometerMeasurement } from 'expo-sensors';
 import { useState, useEffect } from 'react';
 
 let start = Date.now()
+
+type AccelerometerData = AccelerometerMeasurement & {diff: number};
 
 export default function App() {
     const [{ x, y, z, timestamp }, setData] = useState<AccelerometerMeasurement>({
@@ -13,25 +15,45 @@ export default function App() {
         timestamp: 0
     });
     const [timediff, setTimediff] = useState<number>(0.0);
+    const [sub, setSub] = useState<any>(null);
+    const [hist, setHist] = useState<AccelerometerData[]>([]);
 
-    const updateinterval = 1; // ms
+    const updateinterval = 0; // ms
 
     Accelerometer.setUpdateInterval(updateinterval);
-    Accelerometer.addListener(setData);
+
+    const onStartButtonPress = () => {
+        setSub(Accelerometer.addListener(setData));
+    }
+
+    const onStopButtonPress = () => {
+        if (sub) {
+            sub.remove();
+        }
+    }
+
+    const onLogButtonPress = () => {
+        console.log(hist);
+    }
 
     useEffect(() => {
         const diff = (Date.now() - start) / 1000;
         start = Date.now();
         setTimediff(diff);
         // console.log(diff, timestamp, start);
+        // console.log(x, y, z, diff);
+        setHist([...hist, { x, y, z, timestamp, diff } ]);
     }, [x, y, z, setData])
 
     return (
         <View style={styles.container}>
-            <Text>Timediff: {timediff}s</Text>
+            <Button title="Start" onPress={onStartButtonPress} />
+            {/*<Text>Timediff: {timediff}s</Text>
             <Text>x: {x}</Text>
             <Text>y: {y}</Text>
-            <Text>z: {z}</Text>
+            <Text>z: {z}</Text>*/}
+            <Button title="Stop" onPress={onStopButtonPress} />
+            <Button title="Log" onPress={onLogButtonPress} />
         </View>
     );
 }
